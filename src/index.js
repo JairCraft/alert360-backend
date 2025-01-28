d713b636eb622edd7a85c3446e99711dc6d89434
Jair Guanotoa <jairguanotoa@hotmail.com>
2025-01-28 18:26:57 -0500

    feat: :construction: index api message send

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
 });
